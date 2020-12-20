af874bcb2c25a3bcf4dae09fdeea9c093644688d
abhishek <alphaq1205@gmail.com>
2020-12-20 17:05:25 +0530

    .

--- iotDash/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Devices, Fields, DeviceData
-# Register your models here.
-
-admin.site.register(Devices)
-admin.site.register(Fields)
-admin.site.register(DeviceData)
--- iotDash/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IotdashConfig(AppConfig):
-    name = 'iotDash'
